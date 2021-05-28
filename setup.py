1fe628bb249ef1bdeda359de803cea5a53bc191c
Nicolai Haug <nicolaihaug@yahoo.com>
2021-05-28 16:14:41 +0200

    Edit setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,65 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+
 from setuptools import find_packages, setup
 
+here = os.path.abspath(os.path.dirname(__file__))
+
 # Package meta-data.
-NAME = 'pylfi'
-DESCRIPTION = 'Likelihood-free inference with Python.'
-URL = 'https://github.com/nicolossus/master-thesis'
-EMAIL = 'nicolai.haug@fys.uio.no'
-AUTHOR = 'Nicolai Haug'
+NAME = "pylfi"
+DESCRIPTION = "Likelihood-free inference with Python."
+URL = "https://github.com/nicolossus/neuromodels"
+EMAIL = "prof.haug@gmail.com"
+AUTHOR = "Nicolai Haug"
 REQUIRES_PYTHON = '>=3.8.0'
-VERSION = '0.1.0'
+
+REQUIRES_INSTALL = [
+    "numpy",
+    "matplotlib",
+    "scipy",
+    "sklearn",
+]
+
+REQUIRES_EXTRAS = {
+    "dev": [
+        "pytest",
+        "pytest-cov",
+        "flake8>=3.9.2",
+        "isort",
+        "twine",
+    ],
+}
+
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
 
 about = {}
-about['__version__'] = VERSION
+with open(os.path.join(here, NAME, "__version__.py")) as f:
+    exec(f.read(), about)
+
+VERSION = about['__version__']
+
 
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=VERSION,
     description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
+    packages=find_packages(exclude=["tests", ]),
     python_requires=REQUIRES_PYTHON,
-    packages=find_packages(
-        exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    setup_requires=["setuptools>=18.0"],
-    install_requires=["numpy", "matplotlib", "scipy", "sklearn"],
+    install_requires=REQUIRES_INSTALL,
+    extras_require=REQUIRES_EXTRAS,
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
