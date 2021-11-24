4df80b939a466ec480cd181f44bb3eb6d8ff9a26
Nicolai Haug <nicolaihaug@yahoo.com>
2021-11-24 15:59:07 +0100

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import sys
 import sphinx_gallery
 from matplotlib.sphinxext import plot_directive
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
